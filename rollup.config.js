995ef5f4a81d9cfe8319482eb0dbc179e646890f
Jessie Liauw A Fong <jessielaff@live.nl>
2020-08-17 10:59:13 +0200

    Add resolver for @ in vue components

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
 import vue from 'rollup-plugin-vue'
+import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import buble from '@rollup/plugin-buble'
+import alias from '@rollup/plugin-alias'
+import resolve from '@rollup/plugin-node-resolve'
+
+const customResolver = resolve({
+  extensions: ['.mjs', '.js', '.vue', '.jsx', '.json', '.sass', '.scss']
+})
+const projectRootDir = path.resolve(__dirname)
 
 export default {
   input: 'src/main.js',
@@ -11,6 +19,15 @@ export default {
   },
   plugins: [
     commonjs(),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: path.resolve(projectRootDir, 'src')
+        }
+      ],
+      customResolver
+    }),
     vue({
       template: {
         isProduction: true
